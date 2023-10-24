a708494ed0db1c68b937876e24e09ee84be85d47
ankit dubey <ankitsarar142@gmail.com>
2023-10-24 15:49:11 +0530

    "update"

--- src/com/my_new_java/JAVA_Basics/JavaBasic_2.java
+++ src/com/my_new_java/JAVA_Basics/JavaBasic_2.java
@@ -1,6 +1,8 @@
 package com.my_new_java.JAVA_Basics;
 
 public class JavaBasic_2 {
+
+    /*Write a program for a Single line comment, multi-line and documentation comments */
     public static void main(String[] args) {
         int num = 10;
         int num2 = 20;
